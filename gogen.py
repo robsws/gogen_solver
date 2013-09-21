b9af3b5865bb6bb877890049deda6d9878ab9d23
Robert Streeting <robsws@gmail.com>
2013-09-21 19:23:20 +0100

    Added basic file read and main method.

--- gogen.py
+++ gogen.py
@@ -1,1 +1,14 @@
-#!/bin/python
+import sys
+
+usage = "Usage: python gogen filename"
+
+if sys.argc != 1:
+	print usage
+
+for filename in sys.argv:
+	try:
+		puzzle_file = open(filename, 'r')
+	except IOError as error:
+		print "I/O error({0}): {1}".format(error.errno, error.strerror)
+
+
